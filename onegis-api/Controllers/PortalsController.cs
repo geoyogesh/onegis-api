e670dac0eb73904e8939cf99ff48bc5a86e7ff73
geoyogesh <geoyogesh@gmail.com>
2015-07-10 22:29:28 -0400

    First Commit

---
+++ onegis-api/Controllers/PortalsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace onegis_api.Controllers
+{
+    public class PortalsController : ApiController
+    {
+        [Route("sharing/rest/portals/self")]
+        [HttpGet]
+        public String Self()
+        {
+            return "hi";
+        }
+    }
+}
